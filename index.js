2c328ebc3f32bdcfb2027a0beba3ba99c0b2dc68
md5login <md5login@gmail.com>
2021-01-07 10:07:31 +0200

    Migration to github

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import router from "./src/router/Router.js";
 import server from "./src/server/Server.js";
 import app from './src/app/App.js';
-import endpoint from "./src/router/Endpoint.js";
 
 export const Router = router;
 export const Server = server;
 export const App = app;
-export const Endpoint = endpoint;
--- src/router/Endpoint.js
+++
@@ -1,3 +0,0 @@
-export default class Endpoint {
-    static namespace = '/';
-}
