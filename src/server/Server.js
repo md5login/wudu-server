a50f4187b7a26b55467597cfcab2bdd948313880
md5lo <md5login@gmail.com>
2021-12-20 23:00:36 +0200

    Redirect to HTTPS flag

--- src/server/Server.js
+++ src/server/Server.js
@@ -50,6 +50,12 @@ const runServer = initParams => {
             }, (...a) => listener(...a));
             port = 3000;
     }
+    if (initParams.redirectToHttps && initParams.protocol === Server.HTTPS) {
+        http.createServer(function (req, res) {
+            res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+            res.end();
+        }).listen(80);
+    }
     server.keepAliveTimeout = initParams.keepAliveTimeout || 5000;
     port = initParams.port || port;
     server.listen(port);
